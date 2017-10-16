f24ba23a7cc497a1e8244cab19879a0e6ebc5afd
iolumbaram <hazel77744@gmail.com>
2017-10-16 16:13:41 +0800

    Emit RN

--- random.js
+++ random.js
@@ -15,73 +15,34 @@ var gps = {
              lat: null,
              lng: null,
 };
+var randomNum;
 
 var rn = require('random-number');
 var gen = rn.generator({
-  min:  -1000
-, max:  1000
+  min:  -10
+, max:  100
 , integer: true
 })
-gen() // example outputs → -350 
 
-server.route({
-  method: 'POST',
-  path: '/gps_lat',
-  handler: function (request, reply) {
-    const body = request.payload.lat
-    const response = reply(`You sent: ${body} to Hapi`)
-   //console.log(JSON.stringify(request.headers));
-   //console.log(body);
-   //console.log(body);
-    response.header('Content-Type', 'text/plain')
-    gps.lat = body;
-    io.sockets.emit('gps', gps);
-}
-});
+setInterval(function(){
+    console.log('emitting ');
+    randomNum = gen();
+    console.log(randomNum);
+    }, 3000);
+
+
+// io.on('connection', function (socket) {
+//   socket.on('lamppost_dimmer', function (data) {
+//       socket.emit('ui_status', data);
+//   });
+// });
+
 
 io.on('connection', function (socket) {
-  socket.on('floodSelected', function (data) {
-  var id = "C0L2M1N0S1fld";  //always L2 for now
-
-    if (data == true)
-   { //check if cmd (capping) running. if yes, do nothing. if not, start it
-    fs.writeFile("cmdFlood2", "1", function(err) {
-        if(err) {
-            return console.log(err);
-        }
-    });
-
-   }
-  else
-   { //check if cmd running. if yes, kill it. if not, do nothing
-    fs.writeFile("cmdFlood2", "0", function(err) {
-        if(err) {
-            return console.log(err);
-        }
-    });
-   }
+    
+    socket.emit('randomNum', randomNum);
+    console.log('emitted')
   });
-});
-
-io.on('connection', function (socket) {
-  socket.on('lamppost_dimmer', function (data) {
-  console.log('lamp light dimmer btn at: ')
-  console.log(data);
-
-  var id = "C0L"+data.id+"M0N1S0lam";
-  var id_state = data.dimmer_state;
-  console.log(data.dimmer_state);
-  var insertThis = id+":"+id_state;
-
-  socket.emit('ui_status', data);
-  fs.writeFile("ui_status", insertThis, function(err) {
-          if(err) {
-              return console.log(err);
-          }
-      });
-
-  });
-});
 
 
 // To surf webpage
